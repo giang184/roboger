d9db36bbb7ea165c37a19b76ad4de489247b0911
Andrew Giang <giang184@gmail.com>
2021-06-11 09:55:30 -0700

    add more specs to READ.md

--- README.md
+++ README.md
@@ -1,13 +1,33 @@
 Describe: roboger()
 
-Test: "Numbers that contain a 1 will be replaced with "Beep!"
+Test: "Number 1 will be replaced with "Beep!"
 Code: roboger(1);
 Expected Output: "Beep!"
 
-Test: "Numbers that contain a 2 will be replaced with "Boop!"
+Test: "Number 2 will be replaced with "Boop!"
 Code: roboger(2);
 Expected Output: "Boop!"
 
-Test: "Numbers that contain a 3 will be replaced with "Won't you be my neighbor!"
+Test: "Number 3 will be replaced with "Won't you be my neighbor!"
 Code: roboger(3);
+Expected Output: "Won't you be my neighbor!"
+
+Test: "Any numbers that contain a 1 will be replaced with "Beep!"
+Code: roboger(415);
+Expected Output: "Beep!"
+
+Test: "Numbers that contain a 2 will be replaced with "Boop!"
+Code: roboger(592);
+Expected Output: "Boop!"
+
+Test: "Numbers that contain a 3 will be replaced with "Won't you be my neighbor!"
+Code: roboger(3546);
+Expected Output: "Won't you be my neighbor!"
+
+Test: "Numbers that contain a 1 and 3 or 2 and 3 or 1,2 and 3 will be all be replaced with "Won't you be my neighbor!"
+Code: roboger(321);
+Expected Output: "Won't you be my neighbor!"
+
+Test: "Numbers that contain a 1 and 2 will be all be replaced with "Won't you be my neighbor!"
+Code: roboger(12);
 Expected Output: "Won't you be my neighbor!"
--- js/scripts.js
+++ js/scripts.js
@@ -2,50 +2,32 @@
 function roboger(number) {
   if( (number+'').includes('3'))
   {
-    return "Won't you be my neighbor?"
+    return "\"Won't you be my neighbor?\"";
   }
   else if( (number+'').includes('2'))
   {
-    return "Boop!"
+    return "\"Boop!\"";
   }
   else if( (number+'').includes('1'))
   {
-    return "Beep!"
+    return "\"Beep!\"";
   }
   else {
     return number+'';
   }
 }
 
+
 // Business Logic
 function robo(num) {
   let result = '';
-  for(let i = 0; i <= num; i++)
+  for(let i = 0; i < num; i++)
   {
-    if( (i+'').includes('3'))
-    {
-      result += "\"Won't you be my neighbor?\""
-    }
-    else if( (i+'').includes('2'))
-    {
-      result += "\"Boop!\""
-    }
-    else if( (i+'').includes('1'))
-    {
-      result += "\"Beep!\""
-    }
-    else {
-      result += i;
-    }
-    
-    if(i !== num) {
-      result += ", ";
-    }
+    result += roboger(i) + ", ";
   }
-  
+  result += roboger(num);
   return result;
 }
-
 
 
 // UI Logic
@@ -54,7 +36,16 @@ $(document).ready(function(){
     event.preventDefault();
     const number = parseInt($("#number").val());
     
-    $("#robogerMessage").html(robo(number));
-    $(".result").show();
+    if(number<10000)
+    {
+      $("#robogerMessage").html(robo(number));
+      $(".result2").hide();
+      $(".result1").show();
+    }
+    else{
+      $("#brokenMessage").html("The number you entered is too big and it broke my computer! Try again with a smaller number :)");
+      $(".result1").hide();
+      $(".result2").show();
+    }
   });
 });
