f32e940718df21c77e1ec9b8838ab47c1100ef46
Andrew Giang <giang184@gmail.com>
2021-06-11 09:02:31 -0700

    add styling choices to styles.css

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,41 @@
 // Business Logic
-function roboger(num) {
-
+function robo(num) {
+  let result = '';
+  for(let i = 0; i <= num; i++)
+  {
+    if( (i+'').includes('3'))
+    {
+      result += "\"Won't you be my neighbor?\""
+    }
+    else if( (i+'').includes('2'))
+    {
+      result += "\"Boop!\""
+    }
+    else if( (i+'').includes('1'))
+    {
+      result += "\"Beep!\""
+    }
+    else {
+      result += i;
+    }
+    
+    if(i !== num) {
+      result += ", ";
+    }
+  }
+  
+  return result;
 }
+
+
+
+// UI Logic
+$(document).ready(function(){
+  $("form#roboger").submit(function(event){
+    event.preventDefault();
+    const number = parseInt($("#number").val());
+    
+    $("#robogerMessage").html(robo(number));
+    $(".result").show();
+  });
+});
